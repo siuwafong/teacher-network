a54cc0f1bfb56e3e94851ebeda8ca39972f6e9a3
Wilson Fong <wilsonfong82@gmail.com>
2021-01-05 22:11:01 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 5001
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+app.get('/', (req, res) => {
+    res.send("API Running")
+})
